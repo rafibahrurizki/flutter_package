53797906bbb91eb3dc78b4790dae4c787642a614
Muhammad Rafi <rafi.bahrur@micropiranti.com>
2026-09-07 17:46:09 +0700

    all in one

--- calendar_date_picker2/example/lib/main.dart
+++ calendar_date_picker2/example/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:calendar_date_picker2/calendar_date_picker2.dart';
 import 'package:material_ui/material_ui.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
 final today = DateUtils.dateOnly(DateTime.now());
 
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      localizationsDelegates: GlobalMaterialLocalizations.delegates,
+      // localizationsDelegates: GlobalMaterialLocalizations.delegates,
       supportedLocales: const [
         Locale('en', ''),
         Locale('zh', ''),
---
+++ lib/flutter_package.dart
@@ -0,0 +1,10 @@
+library;
+
+export 'package:pinput/pinput.dart';
+export 'package:calendar_date_picker2/calendar_date_picker2.dart';
+export 'package:encrypt/encrypt.dart';
+export 'package:english_words/english_words.dart';
+export 'package:flutter_sticky_header/flutter_sticky_header.dart';
+export 'package:html_unescape/html_unescape.dart';
+export 'package:reorderable_grid/reorderable_grid.dart';
+export 'package:scroll_datetime_picker/scroll_datetime_picker.dart';
